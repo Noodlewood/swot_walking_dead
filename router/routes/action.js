5312fcaee61ee8396eb54ac22d23d10527b4ec61
Noodlewood <daniel.riedmuller@gmail.com>
2015-04-23 14:47:50 +0200

    update

--- router/routes/action.js
+++ router/routes/action.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-
 var tokens = require('../../resources/tokens.js');
 var serverCom = require('../../resources/serverCommunication');
 var func = require('../../resources/functionsInfo.js');
